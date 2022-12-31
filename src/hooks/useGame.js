7f38b9cdb541d89eab4a4ac531b5369996a9beda
Pablo Cabrera <pablofernando54@outlook.com>
2022-12-30 21:03:49 -0600

    Update useGame.js

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -15,7 +15,7 @@ function useGame() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const API_URL = `https://the-trivia-api.com/questions?categories=${selectedCategories
+    const API_URL = `https://the-trivia-api.com/api/questions?categories=${selectedCategories
       .join(',')
       .replaceAll(' ', '_')
       .toLowerCase()}&limit=${questions}`
